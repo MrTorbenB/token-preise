20b56dd799f5f23897adac4d6661e056cb604198
MrTorbenB <140882982+MrTorbenB@users.noreply.github.com>
2024-10-17 10:42:06 +0200

    Update token_price.php

--- token_price.php
+++ token_price.php
@@ -1,12 +1,12 @@
 <?php
-/*
-Plugin Name: Token Preise mit Admin API-Schlüssel-Verwaltung
-Plugin URI: https://torbenb.info/download/
-Description: Zeigt den aktuellen Preis des Pi Tokens auf Huobi und des FONE Tokens auf CoinMarketCap an. Der API-Schlüssel kann im Admin-Bereich verwaltet werden.
-Version: 1.2
-Author: TorbenB
-Author URI: https://torbenb.info/
-*/
+/**
+ * Plugin Name: Token Preise mit Admin API-Schlüssel-Verwaltung
+ * Plugin URI: https://torbenb.info/download/
+ * Description: Zeigt den aktuellen Preis verschiedener Tokens auf CoinMarketCap an. Der API-Schlüssel kann im Admin-Bereich verwaltet werden.
+ * Version: 1.2
+ * Author: TorbenB
+ * Author URI: https://torbenb.info/
+ */
 
 // Menü im Admin-Backend hinzufügen
 function token_prices_add_admin_menu() {
@@ -48,37 +48,23 @@ function token_prices_settings_page() {
     <?php
 }
 
-// Shortcode für PI Token definieren
-function pi_token_price_shortcode() {
-    // Huobi-API-URL für das PI/USDt-Paar
-    $huobi_api_url = 'https://api.huobi.pro/market/detail/merged?symbol=piusdt';
-    
-    // Huobi-API-Aufruf
-    $response = wp_remote_get($huobi_api_url);
-    if (is_wp_error($response)) {
-        return '<p>Fehler beim Abrufen des Pi Token Preises.</p>';
-    }
-    
-    $data = json_decode(wp_remote_retrieve_body($response), true);
-    if (isset($data['tick']['close'])) {
-        $huobi_price = $data['tick']['close'];
-        return '<p>1 PI sind: $' . number_format($huobi_price, 2) . ' USD</p>';
-    } else {
-        return '<p>Preisdaten sind momentan nicht verfügbar.</p>';
-    }
-}
-add_shortcode('pi_token_price', 'pi_token_price_shortcode');
+// Shortcode für Token Preise definieren
+function token_price_shortcode($atts) {
+    $atts = shortcode_atts(
+        array(
+            'symbol' => 'FONE',
+        ),
+        $atts
+    );
 
-// Shortcode für FONE Token definieren
-function fone_token_price_shortcode() {
     $api_key = get_option('token_prices_coinmarketcap_api_key');
 
     if (!$api_key) {
         return '<p>CoinMarketCap API-Schlüssel nicht gesetzt. Bitte setzen Sie den API-Schlüssel in den Einstellungen.</p>';
     }
 
-    // CoinMarketCap-API-URL für FONE/USD
-    $api_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=FONE&convert=USD';
+    // CoinMarketCap-API-URL für das angegebene Token Symbol
+    $api_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=' . strtoupper($atts['symbol']) . '&convert=USD';
 
     $response = wp_remote_get($api_url, array(
         'headers' => array(
@@ -86,15 +72,15 @@ function fone_token_price_shortcode() {
         )
     ));
     if (is_wp_error($response)) {
-        return '<p>Fehler beim Abrufen des FONE Token Preises.</p>';
+        return '<p>Fehler beim Abrufen des ' . esc_html($atts['symbol']) . ' Token Preises.</p>';
     }
 
     $data = json_decode(wp_remote_retrieve_body($response), true);
-    if (isset($data['data']['FONE']['quote']['USD']['price'])) {
-        $price = $data['data']['FONE']['quote']['USD']['price'];
-        return '<p>1 FONE sind: $' . number_format($price, 9) . ' USD</p>';
+    if (isset($data['data'][strtoupper($atts['symbol'])]['quote']['USD']['price'])) {
+        $price = $data['data'][strtoupper($atts['symbol'])]['quote']['USD']['price'];
+        return '<p>1 ' . esc_html($atts['symbol']) . ' sind: $' . number_format($price, 9) . ' USD</p>';
     } else {
-        return '<p>Preisdaten sind momentan nicht verfügbar.</p>';
+        return '<p>Preisdaten für ' . esc_html($atts['symbol']) . ' sind momentan nicht verfügbar.</p>';
     }
 }
-add_shortcode('fone_token_price', 'fone_token_price_shortcode');
+add_shortcode('token_price', 'token_price_shortcode');
